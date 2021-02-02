daf07151e70effdc22a583f0d28d5fb025caace5
Nathan Simpson <email.nsimpson@gmail.com>
2021-02-02 02:52:00 +0100

    so much less code omg

--- clarinet/nets.py
+++ clarinet/nets.py
@@ -1,40 +1,35 @@
-__all__ = ["BayesNet", "_BayesNet", "_BayesNetDefaults"]
+__all__ = ["BayesNet"]
 
 from typing import Dict
 from typing import Any
 
 from functools import singledispatchmethod
-from dataclasses import asdict
-from dataclasses import dataclass
-from dataclasses import field
+from pydantic import BaseModel
 
 from flax.core import freeze, unfreeze, FrozenDict
 
-from .nodes import Node, CategoricalNode, DiscreteNode
+from .nodes import Node, CategoricalNode
 from .validation import validate_node, validate_model_dict
 from .dictutils import nodes_to_dict, modelstring_to_dict
 
 
-@dataclass(frozen=True)
-class _BayesNet:
+class BayesNet(BaseModel):
     nodes: FrozenDict[str, Node]
+    modelstring: str = ""
+    # for pydantic
 
-
-@dataclass(frozen=True)
-class _BayesNetDefaults:
-    modelstring: str = field(default="")
-
-
-@dataclass(frozen=True)
-class BayesNet(_BayesNetDefaults, _BayesNet):
+    class Config:
+        allow_mutation = False
+        arbitrary_types_allowed = True
+        json_encoders = {FrozenDict: lambda t: unfreeze(t)}
 
     @classmethod
-    def from_dict(
+    def from_dict(  # type: ignore
         cls,
         network_dict: Dict[str, Dict[str, Any]],
         validation: bool = True,
         modelstring: str = ""
-    ) -> _BayesNet:
+    ):  # type: ignore
         # validation step
         # TODO: jsonschema
         if validation:
@@ -53,51 +48,33 @@ class BayesNet(_BayesNetDefaults, _BayesNet):
             # special casing
             if "categories" in node_dict.keys():
                 node_dict["categories"] = tuple(node_dict["categories"])
-                nodes[name] = CategoricalNode(name, **node_dict)
+                nodes[name] = CategoricalNode(name=name, **node_dict)
             else:
-                nodes[name] = Node(name, **node_dict)
+                nodes[name] = Node(name=name, **node_dict)
             # display_text = node.display_text or name  # TODO daft
 
         return cls(freeze(nodes), modelstring)
 
     @classmethod
-    def from_modelstring(cls, modelstring: str) -> _BayesNet:
+    def from_modelstring(cls, modelstring: str):  # type: ignore
         return cls.from_dict(modelstring_to_dict(modelstring))
 
     @singledispatchmethod
-    def add_node(self, node) -> _BayesNet:  # type: ignore # noqa
+    def add_node(self, node):  # type: ignore
         raise NotImplementedError(
             f"Type '{type(node)}' of node was not recognised")
 
     @add_node.register
-    def _(self, node: Node) -> _BayesNet:
+    def _(self, node: Node):  # type: ignore
         node_dct = unfreeze(self.nodes)
         name = node.name
         # refactor for node
         node_dct[name] = node
         # make sure to complete arcs from their other ends
         for child in node.children:
-            node_dct[child] = node_dct[child].set_parents(
-                [name],
-                keep_current=True
-            )
+            node_dct[child] = node_dct[child].add_parents([name])
         for parent in node.parents:
-            node_dct[parent] = node_dct[parent].set_children(
-                [name],
-                keep_current=True
-            )
+            node_dct[parent] = node_dct[parent].add_children([name])
         dct = nodes_to_dict(node_dct)
-        validate_node(name, asdict(node), network_dict=dct)
-        return BayesNet.from_dict(dct, validation=False)
-
-    def as_dict(
-        self, serializable: bool = False
-    ) -> Dict[str, Dict[str, Dict[str, Any]]]:
-        net_copy = unfreeze(self.nodes)
-        if serializable:  # TODO: investigate flax serialization of frozendict
-            for name, node in net_copy.items():
-                if isinstance(node, DiscreteNode):
-                    net_copy[name] = node.make_serializable()
-            return {'nodes': nodes_to_dict(net_copy)}
-        else:
-            return {'nodes': nodes_to_dict(net_copy)}
+        validate_node(name, node.dict(), network_dict=dct)
+        return self.__class__.from_dict(dct, validation=False)
--- clarinet/nodes.py
+++ clarinet/nodes.py
@@ -1,125 +1,73 @@
 __all__ = [
-    "_Node",
-    "_NodeDefaults",
     "Node",
-    "_Discrete",
-    "_DiscreteDefaults",
     "DiscreteNode",
-    "_Categorical",
-    "_CategoricalDefaults",
     "CategoricalNode",
 ]
 
 from typing import List
 from typing import Tuple
 from typing import Union
-from typing import Dict
-from typing import Any
 
-from dataclasses import dataclass
-from dataclasses import field
-from dataclasses import asdict
+from pydantic import BaseModel
 
 import jax.numpy as jnp
-from jax.interpreters.xla import _DeviceArray as JaxArray  # type: ignore
+from jax.numpy import ndarray as JaxArray  # type: ignore
+from jax.interpreters.xla import DeviceArray  # type: ignore
 
 
-# funky inheritance pattern to keep defaults seperated from required args. see:
-# https://stackoverflow.com/questions/51575931/class-inheritance-in-python-3-7-dataclasses
-@dataclass(frozen=True)
-class _Node:
+class Node(BaseModel):
     name: str
+    parents: Tuple[str] = ()  # type: ignore
+    children: Tuple[str] = ()  # type: ignore
+    display_text: str = ""
 
+    class Config:
+        allow_mutation = False
 
-@dataclass(frozen=True)
-class _NodeDefaults:
-    # see https://github.com/python/mypy/issues/5738 for ignore reasons
-    parents: Tuple[str] = field(default_factory=tuple)  # type: ignore # noqa
-    children: Tuple[str] = field(default_factory=tuple)  # type: ignore # noqa
-    display_text: str = field(default="")
-
-
-@dataclass(frozen=True)
-class Node(_NodeDefaults, _Node):
-    def set_parents(
+    def add_parents(
         self,
         parents: Union[List[str], Tuple[str]],
-        keep_current: bool = True
-    ) -> _Node:
-        if keep_current:
-            current = list(self.parents)
-            new_parents = tuple(list(parents) + current)
-        else:
-            new_parents = tuple(parents)
+    ) -> 'Node':
+        return self.copy(
+            update={'parents': tuple(parents)},
+            deep=True
+        )
 
-        self_dict = asdict(self)
-        self_dict['parents'] = new_parents
-        return self.__class__(**self_dict)
-
-    def set_children(
+    def add_children(
         self,
         children: Union[List[str], Tuple[str]],
-        keep_current: bool = True
-    ) -> _Node:
-        if keep_current:
-            current = list(self.children)
-            new_children = tuple(list(children) + current)
-        else:
-            new_children = tuple(children)
-
-        self_dict = asdict(self)
-        self_dict['children'] = new_children
-        return self.__class__(**self_dict)
-
-    def as_dict(self) -> Dict[str, Any]:
-        return asdict(self)
+    ) -> 'Node':
+        return self.copy(
+            update={'children': tuple(children)},
+            deep=True
+        )
 
 
-# discrete
-@dataclass(frozen=True)
-class _Discrete(_Node):
-    pass
+class DiscreteNode(Node):
+    prob_table: JaxArray = jnp.array([])
+    # for pydantic
+
+    class Config:
+        allow_mutation = False
+        arbitrary_types_allowed = True
+        json_encoders = {DeviceArray: lambda t: t.tolist()}  # for self.json()
 
 
-@dataclass(frozen=True)
-class _DiscreteDefaults(_NodeDefaults):
-    prob_table: Union[JaxArray, List[float]] = jnp.array([])
-
-
-@dataclass(frozen=True)
-class DiscreteNode(Node, _DiscreteDefaults, _Discrete):
-    def make_serializable(self) -> _Discrete:
-        if isinstance(self.prob_table, list):
-            print(f"Node '{self.name}' is already serializable!")
-            return self
-        self_dict = asdict(self)
-        self_dict['prob_table'] = self_dict['prob_table'].tolist()
-        return self.__class__(**self_dict)
-
-
-# categorical
-@dataclass(frozen=True)
-class _Categorical(_Discrete):
+class CategoricalNode(DiscreteNode):
     categories: Tuple[str]
 
+    class Config:
+        allow_mutation = False
 
-@dataclass(frozen=True)
-class _CategoricalDefaults(_DiscreteDefaults):
-    pass
-
-
-@dataclass(frozen=True)
-class CategoricalNode(DiscreteNode, _CategoricalDefaults, _Categorical):
     @classmethod
     def from_node(
         cls,
         node: Node,
         categories: Union[List[str], Tuple[str]],
         prob_table: JaxArray = jnp.array([])
-    ) -> _Categorical:
-        node_dict = node.as_dict()
+    ) -> 'CategoricalNode':
         return cls(
-            **node_dict,
+            **node.dict(),
             categories=tuple(categories),  # type: ignore
-            prob_table=jnp.array(prob_table)  # just in case...
+            prob_table=prob_table
         )
