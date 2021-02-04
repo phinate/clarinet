64976548b34e9e9edde30ad3e8949babbed22c58
Nathan Simpson <email.nsimpson@gmail.com>
2021-02-04 22:37:36 +0100

    fix serialization of jnp arrays

--- clarinet/nodes.py
+++ clarinet/nodes.py
@@ -8,9 +8,10 @@ __all__ = [
 
 from pydantic import BaseModel, validator
 
+from typing import Any
+
 import jax.numpy as jnp
-from jax.numpy import ndarray as JaxArray  # type: ignore
-from jax.interpreters.xla import DeviceArray  # type: ignore
+from jax.interpreters.xla import _DeviceArray  # type: ignore
 
 
 class Node(BaseModel):
@@ -23,7 +24,7 @@ class Node(BaseModel):
         allow_mutation = False
 
     @validator('parents', 'children')
-    def to_tuple(cls, v: list[str]) -> tuple[str, ...]:
+    def to_tuple(cls, v: Any) -> tuple[str, ...]:
         return tuple(v)
 
     def add_parents(
@@ -46,22 +47,26 @@ class Node(BaseModel):
 
 
 class DiscreteNode(Node):
-    prob_table: JaxArray = jnp.array([])
+    prob_table: _DeviceArray = jnp.array([])
 
     class Config:
         allow_mutation = False
         arbitrary_types_allowed = True
-        json_encoders = {DeviceArray: lambda t: t.tolist()}  # for self.json()
+        json_encoders = {_DeviceArray: lambda t: t.tolist()}  # for self.json()
+
+    @validator('prob_table', pre=True)
+    def to_array(cls, arr: Any) -> _DeviceArray:
+        return jnp.asarray(arr)
 
 
 class CategoricalNode(DiscreteNode):
-    categories: tuple[str]
+    categories: tuple[str, ...]
 
     class Config:
         allow_mutation = False
 
     @validator('categories')
-    def to_tuple(cls, v: list[str]) -> tuple[str, ...]:
+    def to_tuple(cls, v: Any) -> tuple[str, ...]:
         return tuple(v)
 
     @classmethod
@@ -69,7 +74,7 @@ class CategoricalNode(DiscreteNode):
         cls,
         node: Node,
         categories: list[str] | tuple[str, ...],
-        prob_table: JaxArray = jnp.array([])
+        prob_table: _DeviceArray = jnp.array([])
     ) -> CategoricalNode:
         return cls(
             **node.dict(),
