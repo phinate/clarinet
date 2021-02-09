18df8a98c11a07a2106857ddc6d3044285dbdab4
Nathan Simpson <email.nsimpson@gmail.com>
2021-02-09 17:53:01 +0100

    add skeleton for tests

---
+++ tests/nets_test.py
@@ -0,0 +1,17 @@
+# import pytest
+# from clarinet import BayesNet, Node, CategoricalNode, DiscreteNode
+# make sure to use every node type in every example where possible
+
+def test_object_instatiation(params, expected): pass
+
+
+def test_from_modelstring(string, expected): pass
+
+
+def test_from_dict(dct, expected): pass
+
+
+def test_add_node(node, expected): pass
+
+
+def test_modify_nodes(node_types, node_kwargs, expected): pass
