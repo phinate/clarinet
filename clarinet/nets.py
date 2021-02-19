d9730443f7015e65c6846a940834db4becca9fed
Nathan Simpson <email.nsimpson@gmail.com>
2021-02-19 16:10:47 +0100

    feat: add automatic generation of modelstrings for any model init

--- clarinet/nets.py
+++ clarinet/nets.py
@@ -5,7 +5,7 @@ __all__ = ["BayesNet"]
 from typing import Any
 
 from functools import singledispatchmethod
-from pydantic import BaseModel, validator, validate_model
+from pydantic import BaseModel, validator, validate_model, root_validator
 
 from immutables import Map
 
@@ -32,6 +32,15 @@ class BayesNet(BaseModel):
     def dict_to_map(cls, dct: dict[str, Node]) -> Map[str, Node]:
         return Map(dct)
 
+    @root_validator
+    def generate_modelstring(cls, values: dict[str, Any]) -> dict[str, Any]:
+        if values["modelstring"] == "":
+            for node in values["nodes"].values():
+                ps = node.parents
+                values["modelstring"] += f"[{node.name}"
+                values["modelstring"] += f"|{':'.join(ps)}]" if ps else "]"
+        return values
+
     @classmethod
     def from_dict(
         cls,
@@ -51,21 +60,23 @@ class BayesNet(BaseModel):
                 node_dict["parents"] = node_dict["parents"]
             if "children" in node_dict.keys():
                 node_dict["children"] = node_dict["children"]
-            if "name" in node_dict.keys():
-                del node_dict["name"]
-
+            if "name" not in node_dict.keys():
+                node_dict["name"] = name
             # special casing
             if "categories" in node_dict.keys():
                 node_dict["categories"] = node_dict["categories"]
-                nodes[name] = CategoricalNode(name=name, **node_dict)
+                nodes[name] = CategoricalNode(**node_dict)
             else:
-                nodes[name] = Node(name=name, **node_dict)
+                nodes[name] = Node(**node_dict)
             # display_text = node.display_text or name  # TODO daft
         return cls(nodes=nodes, modelstring=modelstring)
 
     @classmethod
     def from_modelstring(cls, modelstring: str) -> BayesNet:
-        return cls.from_dict(modelstring_to_dict(modelstring))
+        return cls.from_dict(
+            modelstring_to_dict(modelstring),
+            modelstring=modelstring
+        )
 
     @singledispatchmethod
     def add_node(self, node):   # type: ignore
