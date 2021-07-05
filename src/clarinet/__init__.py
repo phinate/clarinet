15d258779607a0339297be74f190ba7e998ed8b2
Nathan Simpson <email.nsimpson@gmail.com>
2021-07-05 21:47:20 +0200

    finalise transition

--- src/clarinet/__init__.py
+++ src/clarinet/__init__.py
@@ -1,4 +1,13 @@
-__all__ = ["BayesNet", "CategoricalNode", "DiscreteNode", "Node", "Modelstring"]
+__all__ = [
+    "__version__",
+    "BayesNet",
+    "CategoricalNode",
+    "DiscreteNode",
+    "Node",
+    "Modelstring",
+]
+
+from ._version import version as __version__
 from .modelstring import Modelstring
 from .nets import BayesNet
 from .nodes import CategoricalNode, DiscreteNode, Node
